1ff528cc77db8146168455a5aae9d41f153db895
ondichokev <ondichokev@gmail.com>
2024-08-05 05:41:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob'; 
+}
+
+const leastFavoriteCustomer = 'Charlie';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Alice'; 
+}
+
